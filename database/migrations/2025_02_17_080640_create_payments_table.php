e9970f7e6bf1f5b8e7ec079cf12c43724855facb
Kumar <n88work@gmail.com>
2025-02-17 13:56:37 +0530

    Minor updations

--- database/migrations/2025_02_17_080640_create_payments_table.php
+++ database/migrations/2025_02_17_080640_create_payments_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('medical_payments', function (Blueprint $table) {
+        Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('appointment_id');
             $table->decimal('amount', 8, 2);
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('medical_payments');
+        Schema::dropIfExists('payments');
     }
 };
